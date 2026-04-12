5eff6edd1712b184eb7a9521f1632304fd7a010f
Askebir Tadesse <askebirtadesse@gmail.com>
2026-04-12 21:45:33 +0300

    hero's particles background feature

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
+import ParticlesHero from "./ParticleBackground";
 
 const Hero = () => {
   return (
     <div className="relative h-screen border items-center justify-center border-amber-500 flex text-white overflow-hidden">
+      <ParticlesHero />
+
       <div className="relative z-10 flex flex-col items-center">
         <Image
           src="/images/s1.jpg"
@@ -33,11 +37,16 @@ const Hero = () => {
                 loop: true,
                 delay: 75,
                 deleteSpeed: 30,
-                wrapperClassName: " text-red-500 pl-2",
+                wrapperClassName: " text-cyan-500 pl-2",
               }}
             />{" "}
           </span>
         </h2>
+        <button className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium">
+          {" "}
+          <span>See my work</span>
+          <BsArrowRight className="w-5 h-5 ml-2  inline-block " />{" "}
+        </button>
       </div>
     </div>
   );
