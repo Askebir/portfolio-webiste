46afc2da8dddc9c2924c277f1b049961058c6d77
Askebir Tadesse <askebirtadesse@gmail.com>
2026-04-30 01:57:28 +0300

    Edition after deployment

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -7,11 +7,9 @@ const Contact = () => {
     <div className="pt-16 pb-16">
       <div className="w-[90%] md:w-[80%] lg:w-[70%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center ">
         {/* text content */}
-        <div
-          data-aos="fade-right"
+        <div   data-aos="fade-right"
           data-aos-anchor-placement="top-center"
-          data-aos-delay="0"
-        >
+          data-aos-delay="0" >
           <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200">
             Schedule a call with me to see if I can help
           </h1>
--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-blue-900">
       <h1 className="text-lg text-white/70 text-center">
-        Askebir - Personal Portfolio Website
+        Askebir - Personal Portfolio Website.
       </h1>
     </div>
   );
